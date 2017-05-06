7f958cdf8356e9cc0269f1e8367ab89c60595e07
efthemiosprime <efthemios.suyat@gmail.com>
2017-05-06 00:58:01 -0400

    event stream

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,11 @@
 import Rx from 'rxjs/Rx';
 
+var button = document.querySelector('.button');
+var label = document.querySelector('h2');
+
+var clickStream = Rx.Observable.fromEvent(button, 'click');
+var doubleClickStream = clickStream;
+
+doubleClickStream.subscribe (event => {
+    label.textContent = 'double click';
+})
